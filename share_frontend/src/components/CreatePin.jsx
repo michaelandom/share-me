62ec02b82780ad66ad7ec7bcba01a3c41fe0ab24
michaelandom03 <michaelandom03@gmail.com>
2021-12-29 21:31:44 +0300

    all

--- share_frontend/src/components/CreatePin.jsx
+++ share_frontend/src/components/CreatePin.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState } from "react";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
