84247dbdd4d56a87210a9239f70f70afd4db4882
michaelandom03 <michaelandom03@gmail.com>
2022-01-01 01:09:12 +0300

    login fix

--- share_frontend/src/App.js
+++ share_frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/*" element={<Home />} />
     </Routes>
   );
