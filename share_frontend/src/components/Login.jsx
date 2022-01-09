2164751a5378e1520517d75797b8f9bf92d1d144
michaelandom03 <michaelandom03@gmail.com>
2022-01-09 15:08:18 +0300

    login

--- share_frontend/src/components/Login.jsx
+++ share_frontend/src/components/Login.jsx
@@ -2,7 +2,6 @@ import GoogleLogin from "react-google-login";
 import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
-import logo from "../assets/logowhite.png";
 import { client } from "../client";
 const Login = () => {
     const navigate=useNavigate();
